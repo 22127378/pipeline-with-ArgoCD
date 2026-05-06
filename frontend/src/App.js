44f8b9aecf44595fd68d343ceea6af76bd0a0e27
22127378 <122483062+RavenVelse@users.noreply.github.com>
2026-05-06 22:58:36 +0700

    [CONFIG] Update API URL and Kubernetes configuration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const API_URL = "/api/tasks";
+const API_URL = "http://backend-service/tasks";
 
 const MOCK_DATA = [
   {
